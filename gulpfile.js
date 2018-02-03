2e68aeca78ac7395b837424d24e9efbc2581a5d3
Paweł Kowalczyk <kovalwaffe@gmail.com>
2018-02-03 03:12:09 +0100

    change ES7

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,52 @@
-'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+// optimization of image files (https://tinypng.com/)
+// tinypng = require('gulp-tinypng'),
+// Developer Api Key - https://tinypng.com/
+// tinyApiKey = '',
+const twig = require('gulp-twig');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const rimraf = require('rimraf');
+const htmlbeautify = require('gulp-html-beautify');
+const cache = require('gulp-cache');
+const util = require('gulp-util');
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    notify = require("gulp-notify"),
-    browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    //optimization of image files (https://tinypng.com/)
-    //tinypng = require('gulp-tinypng'),
-    //Developer Api Key - https://tinypng.com/
-    //tinyApiKey = '',
-    twig = require('gulp-twig'),
-    concat = require('gulp-concat'),
-    imagemin = require('gulp-imagemin'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rimraf = require('rimraf'),
-    htmlbeautify = require('gulp-html-beautify'),
-    cache = require('gulp-cache'),
-    util = require('gulp-util');
-
-var config = {
-    js: [
-        'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        'src/js/scripts.js'
-    ],
-    prod: !!util.env.prod
+const config = {
+  js: [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    'src/js/scripts.js',
+  ],
+  prod: !!util.env.prod,
 };
 
-gulp.task('browserSync', function () {
-    browserSync({
-        server: {
-            baseDir: "dist"
-        }
-    });
+gulp.task('browserSync', () => {
+  browserSync({
+    server: {
+      baseDir: 'dist',
+    },
+  });
 });
 
-gulp.task('clean', function () {
-    rimraf.sync('dist');
+gulp.task('clean', () => {
+  rimraf.sync('dist');
 });
 
 require('events').EventEmitter.defaultMaxListeners = 0;
 
-gulp.task('images', function () {
-    gulp.src('src/images/**/*.{jpg,png,gif}')
-        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('dist/images'));
+gulp.task('images', () => {
+  gulp.src('src/images/**/*.{jpg,png,gif}')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'));
 });
-//optimization 'tinypng'
+// optimization 'tinypng'
 /*
  gulp.task('image', () =>
  gulp.src('src/img/*')
@@ -56,63 +54,52 @@ gulp.task('images', function () {
  .pipe(gulp.dest('app/img'))
  );
  */
-gulp.task('sass', function () {
-    return gulp.src('src/sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .on("error", notify.onError(function (error) {
-            return "Error: " + error.message;
-        }))
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions', 'ie >= 9', 'android >= 2.3.3', 'iOS >= 6']
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/css')).pipe(browserSync.reload({
-            stream: true
-        }));
-});
+gulp.task('sass', () => gulp.src('src/sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    outputStyle: 'compressed',
+  }))
+  .on('error', notify.onError(error => `Error: ${error.message}`))
+  .pipe(autoprefixer({
+    browsers: ['last 3 versions', 'ie >= 9', 'android >= 2.3.3', 'iOS >= 6'],
+  }))
+  .pipe(rename({
+    suffix: '.min',
+  }))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.reload({
+    stream: true,
+  })));
 
-gulp.task('scripts', function () {
-    return gulp.src(config.js)
-        .pipe(sourcemaps.init())
-        .pipe(concat('scripts.min.js'))
-        .pipe(config.prod ? uglify() : util.noop())
-        .on("error", notify.onError(function (error) {
-            return "Error: " + error.message;
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/js')).pipe(browserSync.reload({
-            stream: true
-        }));
-});
+gulp.task('scripts', () => gulp.src(config.js)
+  .pipe(sourcemaps.init())
+  .pipe(concat('scripts.min.js'))
+  .pipe(config.prod ? uglify() : util.noop())
+  .on('error', notify.onError(error => `Error: ${error.message}`))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.reload({
+    stream: true,
+  })));
 
-gulp.task('views', function () {
-    return gulp.src('src/views/pages/*.twig')
-        .pipe(twig())
-        .pipe(htmlbeautify())
-        .on("error", notify.onError(function (error) {
-            return "Error: " + error.message;
-        }))
-        .pipe(gulp.dest('dist')).on('end', function () {
-            browserSync.reload();
-        });
-});
+gulp.task('views', () => gulp.src('src/views/pages/*.twig')
+  .pipe(twig())
+  .pipe(htmlbeautify())
+  .on('error', notify.onError(error => `Error: ${error.message}`))
+  .pipe(gulp.dest('dist'))
+  .on('end', () => {
+    browserSync.reload();
+  }));
 
-gulp.task('fonts', function () {
-    return gulp.src('src/fonts/**/*.*')
-        .pipe(gulp.dest('dist/fonts'));
-});
+gulp.task('fonts', () => gulp.src('src/fonts/**/*.*')
+  .pipe(gulp.dest('dist/fonts')));
 
 gulp.task('build', ['clean', 'sass', 'fonts', 'scripts', 'images', 'views']);
 
-gulp.task('default', ['build', 'browserSync'], function () {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['scripts']);
-    gulp.watch('src/images/**/*.{jpg,png,gif}', ['images']);
-    gulp.watch('src/views/**/*.twig', ['views']);
+gulp.task('default', ['build', 'browserSync'], () => {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/images/**/*.{jpg,png,gif}', ['images']);
+  gulp.watch('src/views/**/*.twig', ['views']);
 });
